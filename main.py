a500ae55a04fe84110213f44f43482e23942a2c7
Thanoon Younis <engthanoon1@gmail.com>
2025-01-24 13:08:00 +0300

    make saved images same ass app path

--- main.py
+++ main.py
@@ -478,7 +478,9 @@ def get_pdf_path():
     root.withdraw()  # Hide the root window
     root.attributes('-topmost', True)  # Make the dialog appear on top
     file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
-    return file_path
+    pdf_name = os.path.splitext(os.path.basename(file_path))[0]
+    return file_path, pdf_name
+
 def pdf_to_images(pdf_path):
     pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
     output_folder = os.path.join(os.path.dirname(pdf_path), f"{pdf_name}_students")
@@ -513,7 +515,7 @@ if __name__ == "__main__":
                 student_number_bubbles_count=40
                 total_bubbles_count=answers_bubbles_count+student_number_bubbles_count
                 output_folder=''
-                pdf_path = get_pdf_path()
+                pdf_path,pdf_name = get_pdf_path()
                 pdf_images,output_folder = pdf_to_images(pdf_path)
 
                 # pdf_images=[cv2.imread('20_students/page_1.png'),cv2.imread('20_students/page_2.png'),cv2.imread('20_students/page_3.png'),cv2.imread('20_students/page_4.png'),cv2.imread('20_students/page_5.png'),cv2.imread('20_students/page_6.png'),cv2.imread('20_students/page_7.png'),cv2.imread('20_students/page_8.png'),cv2.imread('20_students/page_9.png'),cv2.imread('20_students/page_10.png'),cv2.imread('20_students/page_11.png'),cv2.imread('20_students/page_12.png'),cv2.imread('20_students/page_13.png'),cv2.imread('20_students/page_14.png'),cv2.imread('20_students/page_15.png'),cv2.imread('20_students/page_16.png'),cv2.imread('20_students/page_17.png'),cv2.imread('20_students/page_18.png'),cv2.imread('20_students/page_19.png'),cv2.imread('20_students/page_20.png'),cv2.imread('20_students/page_21.png'),cv2.imread('20_students/page_22.png')]
@@ -560,7 +562,7 @@ if __name__ == "__main__":
                     student_result, correct_indices = calculate_student_score(student_answers, ANSWER_KEYS)
                     all_choosen_frame=draw_contours_on_frame(img,choosen_answers_bubbles+choosen_std_num_bubbles,color='b')
                     cv2.putText(all_choosen_frame, f"Student: {student_number}, student_result: {student_result}", (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 4)
-                    save_images   ([all_choosen_frame],f'{output_folder}_archive',i+1)
+                    save_images   ([all_choosen_frame],f'{pdf_name}_archive',i+1)
     
                     write_results_to_csv(student_number, student_result, correct_indices,student_answers)                
                     print(f"Student ( {student_number} ) student_result : {student_result}, correct_answers: {correct_indices}")
