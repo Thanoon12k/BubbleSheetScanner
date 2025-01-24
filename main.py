a7b2585f066e00d749b598b78d1ca1ac3dd2a482
Thanoon Younis <engthanoon1@gmail.com>
2025-01-24 11:55:53 +0300

    complete 100% th speed

--- main.py
+++ main.py
@@ -5,19 +5,11 @@ import tkinter as tk
 from tkinter import filedialog
 import shutil
 from utils import *
-
+import time
 def calculate_student_score(student_answers, answer_key):
-
-    # Calculate score by comparing student answers with answer key
-    # Sum 1 point for each correct answer where:
-    # - q_num is less than total student answers (to avoid index errors)
-    # - student answer matches correct answer from key
-    score = sum(1 for q_num, correct_ans in answer_key.items() if  student_answers[q_num] == correct_ans)
-
-    # Get list of question numbers (indices) where student answered correctly
-    # Uses same comparison logic as score calculation
+    student_result = sum(1 for q_num, correct_ans in answer_key.items() if  student_answers[q_num] == correct_ans)
     correct_indices = [q_num for q_num, correct_ans in answer_key.items() if q_num < len(student_answers) and student_answers[q_num] == correct_ans]    
-    return score, correct_indices
+    return student_result, correct_indices
 
 def get_answers_from_xlsx(path):
     data = pd.read_excel(path)
@@ -27,7 +19,8 @@ def get_answers_from_xlsx(path):
                 q=row.iloc[0]
                 answer_key[i] = answer_mapping.get(row.iloc[1], None)  # Handle invalid answers gracefully
     return answer_key
-def write_results_to_csv(student_number, score, correct_indices,student_answers):
+
+def write_results_to_csv(student_number, student_result, correct_indices,student_answers):
         # Define file path
         file_path = 'student_results.csv'
         answer_mapping_reverse = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'}
@@ -36,10 +29,10 @@ def write_results_to_csv(student_number, score, correct_indices,student_answers)
         if os.path.exists(file_path):
             results_df = pd.read_csv(file_path)
         else:
-            results_df = pd.DataFrame(columns=['Student Number'] + ['Score from 100'] + [f'Q{i+1}' for i in range(100)])
+            results_df = pd.DataFrame(columns=['Student Number'] + ['student_result from 100'] + [f'Q{i+1}' for i in range(100)])
 
         # Create a new row for the student
-        new_row = pd.DataFrame([[student_number] + [score] + student_answers_mapped], columns=results_df.columns)
+        new_row = pd.DataFrame([[student_number] + [student_result] + student_answers_mapped], columns=results_df.columns)
         student_number = str(student_number)
         all_students_numbers=results_df['Student Number'].astype(str).values
         
@@ -55,92 +48,64 @@ def write_results_to_csv(student_number, score, correct_indices,student_answers)
 
 
 if __name__ == "__main__":
-            ANSWER_KEYS=get_answers_from_xlsx('answers.xlsx')
-
-
-            # ANSWER_KEYS=get_answers_from_xlsx('answers.xlsx')
-            student_number=0000
-            score=0
-            answers_bubbles=500
-            std_number_bubbles=40
-            total_bubbles=540
-            output_folder=''
-            # pdf_path = get_pdf_path()
-            # pdf_images,output_folder = pdf_to_images('20.pdf')
-            # pdf_images=[cv2.imread('20_students/page_1.png'),cv2.imread('20_students/page_2.png'),cv2.imread('20_students/page_3.png'),cv2.imread('20_students/page_4.png'),cv2.imread('20_students/page_5.png'),cv2.imread('20_students/page_6.png'),cv2.imread('20_students/page_7.png'),cv2.imread('20_students/page_8.png'),cv2.imread('20_students/page_9.png'),cv2.imread('20_students/page_10.png'),cv2.imread('20_students/page_11.png'),cv2.imread('20_students/page_12.png'),cv2.imread('20_students/page_13.png'),cv2.imread('20_students/page_14.png'),cv2.imread('20_students/page_15.png'),cv2.imread('20_students/page_16.png'),cv2.imread('20_students/page_17.png'),cv2.imread('20_students/page_18.png'),cv2.imread('20_students/page_19.png'),cv2.imread('20_students/page_20.png'),cv2.imread('20_students/page_21.png'),cv2.imread('20_students/page_22.png')]
-            pdf_images=[cv2.imread('20_students/page_8.png')]
-            # pdf_images=[cv2.imread('blank.png')]
-            # pdf_images=resize_images(pdf_images,1200)
+                
+                start_time = time.time()
             
+                ANSWER_KEYS=get_answers_from_xlsx('answers.xlsx')
+            
+                answers_bubbles_count=500
+                student_number_bubbles_count=40
+                total_bubbles_count=answers_bubbles_count+student_number_bubbles_count
+                output_folder=''
+                # pdf_path = get_pdf_path()
+                pdf_images,output_folder = pdf_to_images('20.pdf')
 
-            for  i,page in enumerate(pdf_images):
-                student_answers = []
-                std_number_bubbles=[]
-                answers_bubbles=[]
-                student_number = ''
-                min_ratio=0.99
-                max_ratio=1.01
-                adaptive_frame=ggetAdaptiveThresh(page,maxx=55,minn=20)
-                cnts_length=0
-                    
-                while cnts_length<total_bubbles and min_ratio>0:
-                        cnts=getBubblesContours(page,adaptive_frame,540,min_ratio,max_ratio)
-                        
-                        cnts_length=len(cnts)
-                        min_ratio-=0.01
-                        max_ratio+=0.01
-                
-                std_num_cnts     =get_student_num_bubbles(adaptive_frame,cnts)
-                
-                if len(std_num_cnts)<40:
-                    print(f'image  [{i+1}]    - from 40  found {len(std_num_cnts)}  ')
-                    std_num_cnts=fix_missing_num_bubbles(adaptive_frame,std_num_cnts)
-                
-                student_number,std_num_bubbles=find_student_number1(page,adaptive_frame,std_num_cnts,i)
-                print('student_number',student_number)
-                # display_images([draw_contours_on_frame(adaptive_frame,std_num_bubbles)],scale=80)
-                answers_blocks=get_answers_blocks_bubbles(page,adaptive_frame,cnts)
-                new_blocks=[]
-                for blk in answers_blocks:
-                        bb=fix_missing_block_bubbles(adaptive_frame,blk)
-                        new_blocks.append(bb)
-                
-                               # Flatten all blocks into a single list
-                for block in new_blocks:
+                # pdf_images=[cv2.imread('20_students/page_1.png'),cv2.imread('20_students/page_2.png'),cv2.imread('20_students/page_3.png'),cv2.imread('20_students/page_4.png'),cv2.imread('20_students/page_5.png'),cv2.imread('20_students/page_6.png'),cv2.imread('20_students/page_7.png'),cv2.imread('20_students/page_8.png'),cv2.imread('20_students/page_9.png'),cv2.imread('20_students/page_10.png'),cv2.imread('20_students/page_11.png'),cv2.imread('20_students/page_12.png'),cv2.imread('20_students/page_13.png'),cv2.imread('20_students/page_14.png'),cv2.imread('20_students/page_15.png'),cv2.imread('20_students/page_16.png'),cv2.imread('20_students/page_17.png'),cv2.imread('20_students/page_18.png'),cv2.imread('20_students/page_19.png'),cv2.imread('20_students/page_20.png'),cv2.imread('20_students/page_21.png'),cv2.imread('20_students/page_22.png')]
+                # pdf_images=[cv2.imread('20_students/page_8.png')]
+                # pdf_images=[cv2.imread('blank.png')]
+                # pdf_images=resize_images(pdf_images,1200)
+                if os.path.exists(output_folder):
+                    shutil.rmtree(output_folder)
 
-                    sub_ans,sub_ans_bubbles= find_student_answers1(page,adaptive_frame,block,i)
-                    answers_bubbles+=sub_ans_bubbles
-                    student_answers+=sub_ans
-                # display_images([draw_contours_on_frame(adaptive_frame,answers_bubbles)],scale=35)
+                for  i,img in enumerate(pdf_images):
+                    student_result=0
+                    student_answers = []
+                    student_num_bubbles=[]
+                    answers_bubbles=[]
+                    student_number = ''
+                    adaptive_frame=ggetAdaptiveThresh(img,maxx=55,minn=20)
 
-                answers_cnts = [bubble for block in new_blocks for bubble in block] + std_num_cnts
-                all_bubbles = answers_cnts+ std_num_cnts
+                    total_bubbles=getBubblesContours(img,adaptive_frame,total_bubbles_count)
+                
+                    student_num_bubbles=get_student_num_bubbles(adaptive_frame,total_bubbles)
+
+                    blocks_answers_bubbles=get_answers_blocks_bubbles(img,adaptive_frame,total_bubbles)
                 
                 
-                print(f'fix numbers image  [{i+1}]    - from {len(std_num_cnts)} - {len(answers_cnts)} = 580  found {len(all_bubbles)}  ')
+                    for i, block in enumerate(blocks_answers_bubbles):
+                            blocks_answers_bubbles[i]=fix_missing_block_bubbles(adaptive_frame,block)
+                            sub_ans,sub_ans_bubbles= find_student_answers1(img,adaptive_frame,blocks_answers_bubbles[i],i)
+                            answers_bubbles+=sub_ans_bubbles
+                            student_answers+=sub_ans
+
+                    all_answers_bubbles = [bubble for block in blocks_answers_bubbles for bubble in block] + student_num_bubbles
+                    all_bubbles = all_answers_bubbles+ student_num_bubbles
                 
-                # print(f'fix numbers image  [{i+1}]    - from 40  found {len(std_num_cnts)}  ')
-                # for c in std_num_cnts:
-                #                   draw_contours_on_frame(adaptive_frame,[c],display=True,color='r')
-  
-   
-                # print(len(std_num_cnts))
-                # aligned_cnts=remove_not_aligned_counters(adaptive_frame,cnts)
-                # print(f'image  [{i+1}]    - from 40  found {len(std_num_cnts)}  min_ratio={min_ratio},max_ratio={max_ratio}')
-                # page=draw_contours_on_frame(adaptive_frame,cnts)
-                # page=draw_rect_top_right_quarter(page)
                 
-                score, correct_indices = calculate_student_score(student_answers, ANSWER_KEYS)
-                ans_num_frame=draw_contours_on_frame(page,answers_bubbles+std_num_bubbles,color='b')
+                    # print(f'fix numbers image  [{i+1}]    - from {len(student_num_bubbles)} - {len(answers_bubbles)} = 580  found {len(total_bubbles)}  ')
                 
-                cv2.putText(ans_num_frame, f"Student: {student_number}, Score: {score}", (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 4)
+                    student_number,std_num_bubbles=find_student_number1(img,adaptive_frame,student_num_bubbles,i)
                 
-                display_images([ans_num_frame],"adaptive page",scale=37)
-                save_images   ([draw_contours_on_frame(page,answers_bubbles+std_num_bubbles,color='b')],f'{output_folder}_archive',f"[{i+1}]")
+                    student_result, correct_indices = calculate_student_score(student_answers, ANSWER_KEYS)
+                    all_choosen_frame=draw_contours_on_frame(img,answers_bubbles+std_num_bubbles,color='b')
+                    cv2.putText(all_choosen_frame, f"Student: {student_number}, student_result: {student_result}", (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 4)
+                
+                    # display_images([all_choosen_frame],"adaptive img",scale=37)
+                    save_images   ([draw_contours_on_frame(img,answers_bubbles+student_num_bubbles,color='b')],f'{output_folder}_archive',f"[{i+1}]")
     
-                write_results_to_csv(student_number, score, correct_indices,student_answers)                
-                print(f"Student ( {student_number} ) score : {score}, correct_answers: {correct_indices}")
-
-                # display_student_results(student_number, score, root)
-                
-            # print(f"found results for  {len(pdf_images)} students succsessfully !!")
+                    write_results_to_csv(student_number, student_result, correct_indices,student_answers)                
+                    print(f"Student ( {student_number} ) student_result : {student_result}, correct_answers: {correct_indices}")
+            
+                end_time = time.time()
+                execution_time = end_time - start_time
+                print(f"Total execution time: {execution_time:.2f} seconds")
--- utils.py
+++ utils.py
@@ -34,35 +34,43 @@ def draw_contours_on_frame(frame, contours,color='r',display=False,scale=100):
         display_images([frame])
     return frame
 
-def getBubblesContours(img, adaptiveFrame, expected_count=540,min_ratio=0,max_ratio=0):
-    contours, hierarchy = cv2.findContours(adaptiveFrame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # min_ratio = 0.99
-    # max_ratio = 1.01
-    circular_contours = []
+def getBubblesContours(img, adaptiveFrame, expected_count):
+        min_ratio=0.99
+        max_ratio=1.01  
+        contours=[]
+        while len(contours)<expected_count:
+                contours, hierarchy = cv2.findContours(adaptiveFrame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                
+                circular_contours = []
 
-    for contour in contours:
-        perimeter = cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
-        if len(approx) > 2:  # Assuming circular shapes have more than 3 vertices
-            x, y, w, h = cv2.boundingRect(contour)
-            aspect_ratio = float(h) / w
-            condition1= min_ratio <= aspect_ratio <= max_ratio and cv2.contourArea(contour) > 30  #aspect ration = h/w
-            condition2=not ((x >= img.shape[1] // 4 and y <= img.shape[0] // 2.9) or (y <= img.shape[0] // 7)) # not in top right quarter
-                               
-            if condition1 and condition2:  # Remove small dots
-                circular_contours.append(contour)
+                for contour in contours:
+                    perimeter = cv2.arcLength(contour, True)
+                    approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
+                    if len(approx) > 2:  # Assuming circular shapes have more than 3 vertices
+                        x, y, w, h = cv2.boundingRect(contour)
+                        aspect_ratio = float(h) / w
+                        condition1= min_ratio <= aspect_ratio <= max_ratio and cv2.contourArea(contour) > 30  #aspect ration = h/w
+                        condition2=not ((x >= img.shape[1] // 4 and y <= img.shape[0] // 2.9) or (y <= img.shape[0] // 7)) # not in top right quarter
+                                        
+                        if condition1 and condition2:  # Remove small dots
+                            circular_contours.append(contour)
 
-    contours = circular_contours
-    if contours:
-        average_area = sum(cv2.contourArea(contour) for contour in contours) / len(contours)
-    else:
-        average_area = 0
-    filtered_contours = []
-    for cnt in contours:
-        if average_area / 2 <= cv2.contourArea(cnt) <= average_area * 2:
-            filtered_contours.append(cnt)
-    contours = filtered_contours 
-    return contours
+                contours = circular_contours
+                if contours:
+                    average_area = sum(cv2.contourArea(contour) for contour in contours) / len(contours)
+                else:
+                    average_area = 0
+                filtered_contours = []
+                for cnt in contours:
+                    if average_area / 2 <= cv2.contourArea(cnt) <= average_area * 2:
+                        filtered_contours.append(cnt)
+                contours = filtered_contours 
+        
+      
+                min_ratio-=0.01
+                max_ratio+=0.01
+        
+        return contours
 
 
 def add_miss_bubbles_to_row(fm,ref,row,normlize_value):
@@ -82,10 +90,7 @@ def add_miss_bubbles_to_row(fm,ref,row,normlize_value):
     while len(row)<4:
         for i in range(0,3):
             equal_X=abs(cv2.boundingRect(ref[i])[0]-cv2.boundingRect(row[i])[0]) <normlize_value
-            if equal_X:
-                print('equal: ',i)
-            else:
-                print('not equal: ',i)
+            if not equal_X:
                 new_bubble = ref[i].copy()
                 for point in new_bubble:
                     point[0][1] += int(normlize_value * 2.5)        
@@ -112,6 +117,7 @@ def find_ref_row(fm, cnts, num_columns):
             y_first = y
             # display_images([draw_contours_on_frame(fm, slice, color='g')], scale=33)
             if len(slice) == num_columns:
+                slice=sorted(slice, key=lambda c: cv2.boundingRect(c)[1])
                 return slice
             slice = [c]
 
@@ -142,7 +148,7 @@ def add_answers_miss_bubbles_to_row(fm, row,ref, normlize_value,num_columns):
             equal_X=abs(cv2.boundingRect(ref[i])[0]-cv2.boundingRect(row[i])[0]) <normlize_value
             
             if not equal_X:
-                print(f'missing bubble [{i+1}] ')
+                # print(f'missing bubble [{i+1}] ')
                 new_bubble = ref[i].copy()
                 y_offset = cv2.boundingRect(row[0])[1] - cv2.boundingRect(new_bubble)[1]
                 new_bubble[:, :, 1] += y_offset
@@ -223,7 +229,7 @@ def fix_missing_num_bubbles(bw_img, cnts: list) -> list:
                 i = 0
                 break
 
-        print(f"i->{i} from 40 new_rows= {len(cnts)}")
+        # print(f"i->{i} from 40 new_rows= {len(cnts)}")
     cnts=sorted(cnts, key=lambda c: cv2.boundingRect(c)[1])    
     return cnts
 
@@ -249,7 +255,6 @@ def fix_missing_block_bubbles(bw_img, cnts: list) -> list:
                 i = 0
                 break
 
-        print(f"i->{i} from 40 new_rows= {len(cnts)}")
     
     # display_images([draw_contours_on_frame(bw_img,cnts,color='g')],scale=33)
     return cnts
@@ -327,18 +332,18 @@ def get_answers_blocks_bubbles(pg,fm,cnts):
 
 
 def get_student_num_bubbles(bw_img,cnts:list)->list:
+
     number_cnts=[]
-    cnts = sorted(cnts, key=lambda c: cv2.boundingRect(c)[1])
-    cnts=cnts[:40]
-    # Choose the first 4 contours and sort them by x-coordinate
     bubble_columns=4
     bubbles_rows=10
-    first_four_cnts = sorted(cnts[:bubble_columns], key=lambda c: cv2.boundingRect(c)[0])
     
-    x_first, _, _,_ = cv2.boundingRect(first_four_cnts[0])
-    x_last, _, w, _ = cv2.boundingRect(first_four_cnts[-1])
-
-    normlize_value=w//2
+    cnts=sorted(cnts, key=lambda c: cv2.boundingRect(c)[1])[:40]
+    ref=find_ref_row(bw_img,cnts,4)
+    
+    x_first = cv2.boundingRect(ref[0])[0]
+    x_last = cv2.boundingRect(ref[-1])[0]
+    
+    normlize_value = cv2.boundingRect(ref[0])[2] // 2
     cnts = sorted(cnts, key=lambda c: cv2.boundingRect(c)[0])
 
     first_ten_cnts=sorted(cnts[:bubbles_rows], key=lambda c: cv2.boundingRect(c)[1])
@@ -346,28 +351,14 @@ def get_student_num_bubbles(bw_img,cnts:list)->list:
     _, y_first, _, _ = cv2.boundingRect(first_ten_cnts[0])
     _, y_last, _, _ = cv2.boundingRect(first_ten_cnts[-1])
 
-    
-
-    # display_images([draw_contours_on_frame(bw_img,[first_ten_cnts[0]])],scale=50)
-    # display_images([draw_contours_on_frame(bw_img,[first_ten_cnts[-1]])],scale=50)
-     # remove our of y
     for cntr in cnts:
         _,y,_,_=cv2.boundingRect(cntr)
         if y>= (y_first-normlize_value)  and y<=(y_last+normlize_value):
             number_cnts.append(cntr)
-   
-    # remove our of x
-    # for cntr in cnts:
-    #     x,_,_,_=cv2.boundingRect(cntr)
-    #     if x>= (x_first-normlize_value)  and x<=(x_last+normlize_value):
-    #         number_cnts.append(cntr)
-    # print(f'xfirst: {x_first}      xlast: {x_last}        yfirst: {y_first}  ylast: {y_last}  num_contours: {len(number_cnts)}')
-    # Print the coordinates on the left top corner of the image
-    # font = cv2.FONT_HERSHEY_SIMPLEX
-    # text = f'x_first: {x_first}, x_last: {x_last}, y_first: {y_first}, y_last: {y_last}, num_contours: {len(number_cnts)}'
-    # cv2.putText(bw_img, text, (10, 30), font, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-    # draw_contours_on_frame(bw_img,number_cnts,display=True)
     
+    if len(number_cnts)<40:
+        number_cnts=fix_missing_num_bubbles(bw_img,number_cnts)
+                
     return number_cnts
 
 def draw_rect_on_frame(fm, axis=[],thikness=4): #axis=[x1,x2,x3,x4]
